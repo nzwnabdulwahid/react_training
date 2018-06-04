2d2ff855e337efb424c5eec3b1e29e7f1ff79f2d
Niezwan <nzwnabdulwahid@gmail.com>
2018-06-04 15:33:22 +0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput'
-import UserOutput from './UserOutput'
+
 
 class App extends Component {
 
@@ -20,9 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserInput usernameVal={this.state.username} changeHandler={this.usernameChangeHandle}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
+        <p>test</p>
       </div>
     );
   }
