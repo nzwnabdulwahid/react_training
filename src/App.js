48993cbbcef83b48a4e670a8ed32bd5f81b3a9aa
Niezwan <nzwnabdulwahid@gmail.com>
2018-06-04 18:42:04 +0800

    add burger css, state to build burgers, start to get hungry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {username: "meow"};
-  }
-
-  usernameChangeHandle = (event) => {
-    this.setState({
-      username: event.target.value
-    })
-  }
-
 
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
